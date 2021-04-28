15ba5e183ea8f1c98337860af74b0449c5bc9670
spiritbuilder <olaifatimothy@gmail.com>
2021-04-28 17:41:45 +0100

    create new route

--- index.js
+++ index.js
@@ -11,16 +11,35 @@ app.get('/', (req, res)=>{
 res.status(200).send("welcome to notes app");
 })
 //create a note
-app.post("./todos", async(req, res)=>{
+app.post("/notes", async(req, res)=>{
     try{
-            console.log(req.body)
+            const note  = await pool.query('insert into notes()')
     }catch(err){
             console.log(err)
     }
 
 })
 //get all notes
+app.get("/notes", async(req,res)=>{
+    try {
+        const allnotes = await pool.query('select * from notes');
+    res.json(allnotes)
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+})
 
+//get specific note
+app.get('/notes/:id', async(req, res)=>{
+try {
+    const id = req.params
+    const note = await pool.query('select * from notes where id =$1',[id]);
+    res.json(note)
+} catch (error) {
+    console.log(error)
+}
+})
 
 
 app.listen(5000, ()=>{
